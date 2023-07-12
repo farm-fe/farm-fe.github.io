ab5a06031b39e7f9fb15257045922e025c9d8a44
erkelost <1256029807@qq.com>
2023-07-12 22:57:13 +0800

    chore: update three

--- src/components/StarrySky/index.js
+++ src/components/StarrySky/index.js
@@ -5,35 +5,17 @@ import {
   useGLTF,
   OrbitControls,
   PerspectiveCamera,
+  Environment,
   Stars,
 } from "@react-three/drei";
 import "./index.css";
-// function Model({ url }) {
-//   const { nodes } = useGLTF(url);
-//   return (
-//     <group rotation={[-Math.PI / 2, 0, 0]} position={[0, -7, 0]} scale={7}>
-//       <group rotation={[Math.PI / 13.5, -Math.PI / 5.8, Math.PI / 5.6]}>
-//         <mesh
-//           receiveShadow
-//           castShadow
-//           geometry={nodes.planet002.geometry}
-//           material={nodes.planet002.material}
-//         />
-//         <mesh
-//           geometry={nodes.planet003.geometry}
-//           material={nodes.planet003.material}
-//         />
-//       </group>
-//     </group>
-//   );
-// }
 
 export default function App() {
   return (
     <>
       <div className="bg-container" />
       <Canvas dpr={[1.5, 2]} linear shadows style={{ position: "absolute" }}>
-        {/* <fog attach="fog" args={["#272730", 16, 30]} />
+        <fog attach="fog" args={["#272730", 16, 30]} />
         <ambientLight intensity={0.75} />
         <PerspectiveCamera makeDefault position={[0, 0, 16]} fov={75}>
           <pointLight intensity={1} position={[-10, -25, -10]} />
@@ -46,18 +28,16 @@ export default function App() {
             shadow-mapSize={[1024, 1024]}
             shadow-bias={-0.0001}
           />
-        </PerspectiveCamera> */}
-        {/* <Suspense fallback={null}>
-          <Model url="/scene.glb" />
-        </Suspense> */}
+        </PerspectiveCamera>
         <OrbitControls
           autoRotate
+          autoRotateSpeed={0.05}
           enablePan={true}
           enableZoom={true}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Stars radius={500} depth={50} count={1000} factor={20} />
+        <Stars radius={600} depth={50} count={1000} factor={10} />
       </Canvas>
       <div className="layer" />
       <Loader />
