3721ed8bb90e0de58c493d917e5afd0d769fcf03
brightwu <1521488775@qq.com>
2024-04-24 16:07:58 +0800

    chore: update social card (#84)
    Co-authored-by: brightwwu <brightwwu@tencent.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,7 +92,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/farm-social-card.png",
       navbar: {
         title: "Farm",
         logo: {
