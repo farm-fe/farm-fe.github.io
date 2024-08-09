dfd26197dd35ca2fd97621008ed2a4f1d2e4856c
erkelost <1256029807@qq.com>
2024-08-09 18:08:25 +0800

    chore: update page

--- src/components/MagicUi/card.tsx
+++ src/components/MagicUi/card.tsx
@@ -183,38 +183,6 @@ const SkeletonFive = () => {
     },
   };
 
-  // return (
-  //   <motion.div
-  //     initial="initial"
-  //     whileHover="animate"
-  //     className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2"
-  //   >
-  //     <motion.div
-  //       variants={variants}
-  //       className="flex flex-row rounded-2xl border border-neutral-100 dark:border-white/[0.2] p-2  items-start space-x-2 bg-soft dark:bg-soft"
-  //     >
-  //       <img
-  //         src="https://pbs.twimg.com/profile_images/1417752099488636931/cs2R59eW_400x400.jpg"
-  //         alt="avatar"
-  //         height="100"
-  //         width="100"
-  //         className="rounded-full h-10 w-10"
-  //       />
-  //       <div className="text-xs text-neutral-500">
-  //         There are a lot of cool framerworks out there like React, Angular,
-  //         Vue, Svelte that can make your life ....
-  //       </div>
-  //     </motion.div>
-  //     <motion.div
-  //       variants={variantsSecond}
-  //       className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2 items-center justify-end space-x-2 w-3/4 ml-auto bg-soft dark:bg-soft"
-  //     >
-  //       <div className="text-xs text-neutral-500">Use PHP.</div>
-  //       <div className="h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 flex-shrink-0" />
-  //     </motion.div>
-  //   </motion.div>
-  // );
-
   return (
     <motion.div
       initial="initial"
