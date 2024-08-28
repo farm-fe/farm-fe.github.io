fa7080e4040db88f16e8cd6e3d554dfe281ddcf6
erkelost <1256029807@qq.com>
2024-08-28 15:32:53 +0800

    chore: Adapt mobile terminal

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import type FooterType from '@theme/Footer';
+import { FaXTwitter } from "react-icons/fa6";
 import type { WrapperProps } from '@docusaurus/types';
-import { GitHubLogoIcon, TwitterLogoIcon, DiscordLogoIcon } from '@radix-ui/react-icons';
+import { GitHubLogoIcon, DiscordLogoIcon } from '@radix-ui/react-icons';
 
 type Props = WrapperProps<typeof FooterType>;
 
@@ -20,10 +21,10 @@ export default function FooterWrapper(props: Props): JSX.Element {
                 <p className="text-base text-gray-500 font-semibold mb-4">Guide</p>
                 <ul className="list-none space-y-3 p-0">
                   <li>
-                    <a href="#" title="" className="text-sm sm:text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Quick Start </a>
+                    <a href="/docs/quick-start" title="" className="text-sm sm:text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Quick Start </a>
                   </li>
                   <li>
-                    <a href="#" title="" className="text-sm sm:text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Introduction </a>
+                    <a href="/docs/why-farm" title="" className="text-sm sm:text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Introduction </a>
                   </li>
                 </ul>
               </div>
@@ -32,10 +33,10 @@ export default function FooterWrapper(props: Props): JSX.Element {
                 <p className="text-base text-gray-500 font-semibold mb-4">Community</p>
                 <ul className="list-none space-y-3 p-0">
                   <li>
-                    <a href="#" title="" className="text-sm sm:text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> WeChat Group </a>
+                    <a href="https://github.com/farm-fe/farm?tab=readme-ov-file#chat-with-us" title="" className="text-sm sm:text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> WeChat Group </a>
                   </li>
                   <li>
-                    <a href="#" title="" className="text-sm sm:text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Discord </a>
+                    <a href="https://discord.com/invite/mDErq9aFnF" title="" className="text-sm sm:text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Discord </a>
                   </li>
                 </ul>
               </div>
@@ -44,10 +45,10 @@ export default function FooterWrapper(props: Props): JSX.Element {
                 <p className="text-base text-gray-500 font-semibold mb-4">More</p>
                 <ul className="list-none space-y-3 p-0">
                   <li>
-                    <a href="#" title="" className="text-sm sm:text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Github </a>
+                    <a href="https://github.com/farm-fe/farm" title="" className="text-sm sm:text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Github </a>
                   </li>
                   <li>
-                    <a href="#" title="" className="text-sm sm:text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Twitter </a>
+                    <a href="https://x.com/FarmFe71928" title="" className="text-sm sm:text-base text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"> Twitter </a>
                   </li>
                 </ul>
               </div>
@@ -59,9 +60,15 @@ export default function FooterWrapper(props: Props): JSX.Element {
 
         <div className="flex flex-col items-center sm:flex-row sm:justify-between">
           <ul className="flex items-center space-x-4 mb-4 sm:mb-0 p-0">
-            <GitHubLogoIcon className='h-5 w-5 sm:h-6 sm:w-6' />
-            <TwitterLogoIcon className='h-5 w-5 sm:h-6 sm:w-6' />
-            <DiscordLogoIcon className='h-5 w-5 sm:h-6 sm:w-6' />
+            <a href="https://github.com/farm-fe/farm" target="_blank" rel="noopener noreferrer">
+              <GitHubLogoIcon className='h-5 w-5 sm:h-6 sm:w-6 text-white hover:text-gray-300' />
+            </a>
+            <a href="https://x.com/FarmFe71928" target="_blank" rel="noopener noreferrer">
+              <FaXTwitter className='h-5 w-5 sm:h-6 sm:w-6 text-white hover:text-gray-300' />
+            </a>
+            <a href="https://discord.com/invite/mDErq9aFnF" target="_blank" rel="noopener noreferrer">
+              <DiscordLogoIcon className='h-5 w-5 sm:h-6 sm:w-6 text-white hover:text-gray-300' />
+            </a>
           </ul>
 
           <p className="text-xs sm:text-sm text-center text-gray-100">Copyright © 2024 Farm Community. Built with Docusaurus.</p>
